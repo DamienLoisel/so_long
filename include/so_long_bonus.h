89ac80e8265b9e2bd2be91b530a85b5c2b6d21ac
DamienLoisel <damien.sti2D@gmail.com>
2024-09-15 13:58:57 +0200

    Add files via upload

---
+++ include/so_long_bonus.h
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dloisel <dloisel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 19:37:41 by dloisel           #+#    #+#             */
+/*   Updated: 2024/08/08 18:19:07 by dloisel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_BONUS_H
+# define SO_LONG_BONUS_H
+
+# include "/home/dloisel/sgoinfre/minilibx-linux/mlx.h"
+# include "/home/dloisel/sgoinfre/minilibx-linux/mlx_int.h"
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <time.h>
+# include <sys/time.h>
+
+# define IMG_HEIGHT	32
+# define IMG_WIDTH	32
+
+# define WALL '1'
+# define FLOOR '0'
+# define PLAYER 'P'
+# define COIN 'C'
+# define MAP_EXIT 'E'
+# define ENEMY 'G'
+
+# define ESC_KEY 65307
+# define W_KEY	119
+# define A_KEY	97
+# define S_KEY	115
+# define D_KEY	100
+
+# define UP_KEY	65362
+# define LEFT_KEY	65361
+# define RIGHT_KEY	65363
+# define DOWN_KEY	65364
+
+# define RED "\x1b[31m"
+# define GREEN "\x1b[32m"
+# define YELLOW "\x1b[33m"
+# define BLUE "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN "\x1b[36m"
+# define RESET "\x1b[0m"
+
+# define ANIMATION_UPDATE 0.75
+
+typedef struct s_pos
+{
+	int	x;
+	int	y;
+}	t_pos;
+
+typedef struct s_sprite
+{
+	void	*image[];
+	int		width;
+	int		height;
+}	t_sprite;
+
+typedef struct s_map
+{
+	char	**fullmap;
+	char	**flood_fill;
+	int		rows;
+	int		columns;
+	int		player_count;
+	int		coin_count;
+	int		exit_count;
+	int		enemy_count;
+	int		is_map_valid;
+}	t_map;
+
+typedef struct s_player
+{
+	t_pos	pos;
+}	t_player;
+
+typedef struct s_game
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	int			width;
+	int			height;
+	int			movements_count;
+	int			moving;
+	int			current_frame;
+	t_map		map;
+	t_player	player;
+	t_sprite	wall;
+	t_sprite	coin1;
+	t_sprite	coin2;
+	t_sprite	exit_open;
+	t_sprite	floor;
+	t_sprite	player_sprite;
+	t_sprite	enemy_sprite;
+	t_sprite	enemy_sprite2;
+}	t_game;
+
+void		ft_map_parser(t_game *game);
+void		ft_check_rows(t_game *game);
+void		ft_check_columns(t_game *game);
+void		ft_count_map_parameters(t_game *game, int i, int j);
+void		ft_verify_map_parameters(t_game *game);
+void		ft_error_map(char *msg, t_game *game);
+void		ft_map_init(t_game *game, char *argv, int fd);
+void		ft_check_emptyline(char *line, t_game *game);
+void		ft_ismap_rectangle(char *line, t_game *game);
+int			first_row_length(char *line);
+void		ft_arg_verif(int argc, char **argv, t_game *game);
+void		ft_is_map_beatable(t_game *game);
+void		ft_floodfill(t_game *flood_game, int x, int y);
+void		ft_get_player_pos(t_game *flood_game, t_pos *player_pos);
+void		ft_free_split(t_game *game);
+void		ft_struct_init(t_game *game);
+void		ft_map_main(t_game *game, char **argv);
+void		ft_mlx_init(t_game *game);
+int			window_destroy(t_game *game);
+int			ft_handle_input(int keycode, t_game *game);
+int			ft_render_map(t_game *game);
+t_sprite	ft_load_sprite(t_game *game, char *file_path);
+void		ft_init_sprite(t_game *game);
+void		ft_identify_sprite(t_game *game, int x, int y);
+void		ft_render_sprite(t_game *game, t_sprite sprite, \
+int line, int columns);
+void		ft_render_player(t_game *game, int line, int columns);
+void		ft_free_flood_fill(t_game *game);
+void		ft_player_move(t_game *game, int new_y, int new_x);
+void		ft_destroy_images(t_game *game);
+void		ft_win(t_game *game);
+void		ft_print_movements(t_game *game);
+void		ft_lose(t_game *game);
+void		ft_update_enemy_sprite(t_game *game, int y, int x);
+void		ft_update_coin_sprite(t_game *game, int y, int x);
+void		ft_enemy_detect(t_game *game);
+int			ft_enemy_direction(t_game *game, int y, int x);
+int			ft_enemy_move(t_game *game, int y, int x);
+
+#endif
